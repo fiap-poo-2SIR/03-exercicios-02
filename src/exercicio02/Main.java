9d90989d055aee9595d05b1d2ca49976e5facfbd
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-27 22:46:06 -0300

    enviando o exercício 03

---
+++ src/exercicio02/Main.java
@@ -0,0 +1,35 @@
+package exercicio02;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("0.000");
+        Scanner sc = new Scanner(System.in);
+        Ponto p1 = new Ponto();
+        Ponto p2 = new Ponto();
+
+        // entrada de dados
+        System.out.println("Informe as coordenadas do ponto 1:");
+        p1.x = sc.nextInt();
+        p1.y = sc.nextInt();
+        System.out.println("Informe as coordenadas do ponto 2:");
+        p2.x = sc.nextInt();
+        p2.y = sc.nextInt();
+
+        // impressão da distância entre os dois pontos
+        System.out.println("distância do p1 para o p2: " + df.format(p1.calcularDistancia(p2)));
+        System.out.println("distância do p2 para o p1: " + df.format(p2.calcularDistancia(p1)));
+
+        // impressão da distância de cada ponto até a origem
+        System.out.println();
+        System.out.println("distância do p1 até a origem: " + df.format(p1.calcularDistanciaAteOrigem()));
+        System.out.println("distância do p2 até a origem: " + df.format(p2.calcularDistanciaAteOrigem()));
+
+        // impressão do ponto mais próximo da origem
+        System.out.println();
+        Ponto aux = p1.maisProximoDaOrigem(p1, p2);
+        System.out.println("ponto mais próximo: " + aux.formatarPonto());
+    }
+}
---
+++ src/exercicio02/Ponto.java
@@ -0,0 +1,37 @@
+package exercicio02;
+
+public class Ponto {
+    // variáveis de instância ou atributos
+    int x;
+    int y;
+
+    // método para calcular e retornar a distância entre dois pontos
+    public double calcularDistancia(Ponto p) {
+        double distancia;
+        distancia = Math.sqrt(Math.pow(x - p.x, 2) + Math.pow(y - p.y, 2));
+        return distancia;
+    }
+
+    // método para calcular e retornar a distância de um ponto até a origem
+    public double calcularDistanciaAteOrigem() {
+        double distancia;
+        Ponto origem = new Ponto();
+        distancia = calcularDistancia(origem);
+        return distancia;
+    }
+
+    // método para retornar os dados do objeto formatado
+    public String formatarPonto() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    // método para retornar o ponto mais próximo da origem
+    public Ponto maisProximoDaOrigem(Ponto p1, Ponto p2) {
+        double dp1 = p1.calcularDistanciaAteOrigem();
+        double dp2 = p2.calcularDistanciaAteOrigem();
+        if (dp1 < dp2) {
+            return p1;
+        }
+        return p2;
+    }
+}
